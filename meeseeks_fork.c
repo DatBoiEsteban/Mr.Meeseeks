cc6babdcd268593d09f1d19f5097d80dc5e883df
Esteban Mata <esteban1113001@gmail.com>
2021-04-22 23:48:10 -0600

    la mate funca

--- meeseeks_fork.c
+++ meeseeks_fork.c
@@ -2,8 +2,10 @@
 #include <sys/wait.h>
 #include <unistd.h>
 #include <time.h>
+#include <string.h>
 
 #include "globales.c"
+#include "general.c"
 
 char* realizarTarea(char* tarea, double dificultad) {
     pid_t pid;
@@ -55,32 +57,58 @@ char* realizarTarea(char* tarea, double dificultad) {
     return ""; // La salida formateada para la bitacora
 }
 
-char* ejecutarPrograma(char *programa) {
+char* ejecutarPrograma(char *programa, char *arg1, char *arg2) {
+    
     clock_t inicio_op = clock();
     double tiempo_op;
-
     int fd[2];
     generatePipe(fd);
-
     int pid = fork();
+    
+
     if(pid == 0) {
-        // aja lo de correr esta mica
-        writeToPipe(fd, "");// Agregar el mensaje
+        int fallo = run(programa, arg1, arg2);
+
+        if(fallo == -1) {
+            writeToPipe(fd, "NO");
+        } else {
+            writeToPipe(fd, "YES");
+        }
+        
+        exit(0);
+
     } else {
+        char *mensaje = malloc(sizeof(char) * 512), *recibido = malloc(sizeof(char)*30), *tiempo = malloc(sizeof(char)*30);
+
         if(pid < 0) {
             //se despicho el fork
-
-
         } else {
             // Meeseeks Box
-            wait(pid);
+            wait(&pid);
             tiempo_op = (double)(clock() - inicio_op) / CLOCKS_PER_SEC;
-
-            readFromPipe(fd, ""); // Agregar donde recibir el mensaje
+            sprintf(tiempo, "%f.4", tiempo_op);
+            readFromPipe(fd, recibido); // Agregar donde recibir el mensaje
         }
-    }
+        strcat(mensaje, "Program executed: ");
+        strcat(mensaje, programa);
 
-    return ""; // La salida formateada para la bitacora
+        if(arg1 != "") {
+            strcat(mensaje, " ");
+            strcat(mensaje, arg1);
+        }
+
+        if(arg2 != "") {
+            strcat(mensaje, " ");
+            strcat(mensaje, arg2);
+        }
+
+        strcat(mensaje, ", Successful?: ");
+        strcat(mensaje, recibido);
+        strcat(mensaje, ", Time of execution: ");
+        strcat(mensaje, tiempo);
+        strcat(mensaje, "\n");
+        return mensaje; // La salida formateada para la bitacora
+    }
 }
 
 char* hacerLaMate(char* exp) {
@@ -91,19 +119,33 @@ char* hacerLaMate(char* exp) {
     generatePipe(fd);
     int pid = fork();
     if(pid == 0) {
-        // aja lo de correr esta mica
-        writeToPipe(fd, "");// Agregar el mensaje
+        
+        double resultado = eval(exp);
+        char *resulSTR = malloc(sizeof(char) * 30);
+        sprintf(resulSTR,"%f.4", resultado);
+        writeToPipe(fd, resulSTR);// Agregar el mensaje
+        exit(0);
     } else {
+        char *mensaje = malloc(sizeof(char) * 512), *recibido = malloc(sizeof(char)*30), *tiempo = malloc(sizeof(char)*30);
+
         if(pid < 0) {
             //se despicho el fork
-
+            printf("crome");
         } else {
             // Meeseeks Box
-            wait(pid);
+            wait(&pid);
             tiempo_op = (double)(clock() - inicio_op) / CLOCKS_PER_SEC;
-
-            readFromPipe(fd, ""); // Agregar donde recibir el mensaje
+            sprintf(tiempo, "%f.4", tiempo_op);
+            readFromPipe(fd, recibido); // Agregar donde recibir el mensaje
         }
+        strcat(mensaje, "Expression evaluated: ");
+        strcat(mensaje, exp);
+        strcat(mensaje, ", Value: ");
+        strcat(mensaje, recibido);
+        strcat(mensaje, ", Time of execution: ");
+        strcat(mensaje, tiempo);
+        strcat(mensaje, "\n");
+        return mensaje; // La salida formateada para la bitacora
     }
 
     return ""; // La salida formateada para la bitacora
